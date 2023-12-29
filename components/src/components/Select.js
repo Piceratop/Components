bce42943488be27d72d9b68deb60954818ba49e8
Piceratop Ultimate <buinhatminh2k52005@gmail.com>
2023-12-29 10:01:17 +0700

    Add dropdown

--- components/src/components/Select.js
+++ components/src/components/Select.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import classNames from "classnames/bind";
 import style from "./Select.scss";
 
 const cx = classNames.bind(style);
 
 export default function Select({ ...props }) {
+   const [isFocused, setIsFocused] = useState(false);
+
+   const handleBlur = () => {
+      setIsFocused(false);
+   };
+
    return (
-      <div className="select-container">
-         <select className={cx("select")} {...props}>
+      <div className={cx("custom-select")}>
+         <select
+            className={cx("select")}
+            onBlur={handleBlur}
+            onClick={() => setIsFocused(!isFocused)}
+            {...props}
+         >
             <option className={cx("option")} value="">
                Select an option
             </option>
@@ -18,7 +29,12 @@ export default function Select({ ...props }) {
                2
             </option>
          </select>
-         <span className="custom-arrow"></span>
+         <span
+            className={cx("custom-arrow", {
+               "is-focused": isFocused,
+               "not-focused": !isFocused,
+            })}
+         ></span>
       </div>
    );
 }
